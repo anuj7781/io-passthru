d5b1a62ab6d21154d037cb6c9a80ee7a6120dbcc
Anuj Gupta <anuj20.g@samsung.com>
2024-01-12 14:13:37 +0530

    benchmark: refactor multi device script to take two char and block device
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>

---
+++ benchmark/measure-two-devices.sh
@@ -0,0 +1,63 @@
+dev_char1_str=$1;
+dev_char2_str=$2;
+dev_blk1_str=$3;
+dev_blk2_str=$4;
+
+run()
+{
+	local pt=$1;
+	local od=$2;
+	local dev_str;
+	local cs;
+
+	core_str='0,1';
+	if [ $pt -eq 1 ]
+	then
+		cs=' -O0 -u1';
+		of='pt';
+		dev_str="${dev_char1_str} ${dev_char2_str}";
+	else
+		cs=' -O1';
+		of='blk';
+		dev_str="${dev_blk1_str} ${dev_blk2_str}";
+	fi
+
+	printf "Test: ${dev_str} on cpu ${core_str}\n"
+
+	kstr="taskset -c $core_str t/io_uring -r4 -b512 -d128 -c32 -s32 -p0 -F1 -B0 -P0 $cs -n2 $dev_str"
+	echo $kstr
+	if [ $pt -eq 1 ]
+	then
+		printf "For ${dev_str} char device\n" > ${od}/$of
+	else
+		printf "For ${dev_str} block device \n" > ${od}/$of
+	fi
+
+	printf "\nconfig = plain\n" >> ${od}/$of
+
+	taskset -c $core_str t/io_uring -r4 -b512 -d128 -c32 -s32 -p0 -F1 -B0 -P0 $cs -n2 $dev_str >> ${od}/$of
+
+	printf "\nconfig = plain + fb\n" >>  ${od}/$of
+
+	taskset -c $core_str t/io_uring -r4 -b512 -d128 -c32 -s32 -p0 -F1 -B1 -P0 $cs -n2 $dev_str >> ${od}/$of
+
+	printf "\nconfig = plain + iopoll\n" >> ${od}/$of
+
+	taskset -c $core_str t/io_uring -r4 -b512 -d128 -c32 -s32 -p1 -F1 -B0 -P0 $cs -n2 $dev_str >> ${od}/$of
+
+	printf "\nconfig = plain + iopoll + fb\n" >> ${od}/$of
+
+	taskset -c $core_str t/io_uring -r4 -b512 -d128 -c32 -s32 -p1 -F1 -B1 -P0 $cs -n2 $dev_str >> ${od}/$of
+}
+
+outdir=out-$(date +"%d-%m-%Y-%H-%M-%S")
+mkdir $outdir
+echo "output will be inside the dir $outdir"
+
+#passthru test
+echo "running passthru test on 2 device(s)"
+run 1 $outdir
+
+#block test
+echo "running block test on 2 device(s)"
+run 0 $outdir
